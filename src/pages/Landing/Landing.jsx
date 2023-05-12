ffd43a5f8c1df880def5db3d7b90d80e8485f50a
MikeE <mike.d.enders@gmail.com>
2023-05-12 16:30:37 -0400

    mikes pull

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,6 +8,8 @@ const Landing = () => {
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
       <Link to="/the-well-styled-things">David's Things</Link><br/>
       <Link to="/the-silly-things">Hunter's Things</Link><br/>
+      <Link to="/the-taco-things">Mike's Things</Link><br/>
+
     </>
   )
 }
